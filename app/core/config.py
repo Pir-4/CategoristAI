268937475808ad5029e8710cde8047c2a5ce0831
Valentin Agapitov <valentin.agapitov@exness.com>
2026-03-14 15:45:54 +0000

    Set up to read config from env

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,12 @@
 from pydantic import AnyUrl, Field, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class DataBaseSettings(BaseSettings):
+class AppBaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+class DataBaseSettings(AppBaseSettings):
     sql_url: PostgresDsn = Field(validation_alias="DATABASE_URL")
     vector_url: AnyUrl = Field(validation_alias="VECTOR_DB_URL")
 
