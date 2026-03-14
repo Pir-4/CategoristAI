81c9347b39cc70e7e5837033714ac3dbae89ec5e
Valentin Agapitov <valentin.agapitov@exness.com>
2026-03-14 14:05:15 +0000

    Change structure configs

--- app/core/config.py
+++ app/core/config.py
@@ -5,3 +5,13 @@ from pydantic_settings import BaseSettings
 class DataBaseSettings(BaseSettings):
     sql_url: PostgresDsn = Field(validation_alias="DATABASE_URL")
     vector_url: AnyUrl = Field(validation_alias="VECTOR_DB_URL")
+
+
+class AppSettings:
+    data_base: DataBaseSettings
+
+    def __init__(self):
+        self.data_base = DataBaseSettings()
+
+
+settings = AppSettings()
