e7c2c80d30739d581887afc7b9e6dc4849bbe1aa
Valentin Agapitov <valentin.agapitov@exness.com>
2026-03-21 12:31:46 +0000

    Add logs to funcs

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import FastAPI
 
 from app.api import v1_routers
@@ -6,5 +8,9 @@ from app.core import setup_logging
 app = FastAPI()
 setup_logging()
 
+logger = logging.getLogger(__name__)
+
+logger.info("Starting CategoristAI...")
 for router in v1_routers:
     app.include_router(router)
+logger.info("Routers connected.")
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,3 +1,4 @@
+import logging
 from uuid import UUID
 
 from sqlalchemy import select
@@ -7,17 +8,22 @@ from app.core import hash_password
 from app.models import User
 from app.schemas import UserCreate, UserUpdate
 
+logger = logging.getLogger(__name__)
+
 
 async def get_user(session: AsyncSession, user_id: UUID) -> User | None:
+    logger.info("Get user by id: %s", user_id)
     return await session.get(User, user_id)
 
 
 async def get_users(session: AsyncSession) -> list[User]:
+    logger.info("Get all users")
     result = await session.execute(select(User))
     return list(result.scalars().all())
 
 
 async def create_user(session: AsyncSession, data: UserCreate) -> User:
+    logger.info("Creating user with login: %s", data.login)
     new_user = User(
         login=data.login, hashed_password=hash_password(data.password)
     )
@@ -30,6 +36,7 @@ async def create_user(session: AsyncSession, data: UserCreate) -> User:
 async def update_user(
     session: AsyncSession, user_id: UUID, data: UserUpdate
 ) -> User | None:
+    logger.info("Updating user %s", user_id)
     user = await get_user(session, user_id)
     if not user:
         raise ValueError(f"User with id {user_id} not found")
