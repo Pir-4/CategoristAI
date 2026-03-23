946be6d625f656e417d0085b845ed9ea6cc1a04a
Valentin Agapitov <valentin.agapitov@exness.com>
2026-03-23 20:10:48 +0000

    Implement hash and verity password

--- app/core/security.py
+++ app/core/security.py
@@ -1,2 +1,14 @@
+import bcrypt
+
+
 def hash_password(plain: str) -> str:
-    return plain  # TODO init hash pass
+    b_plain = plain.encode("utf-8")
+    salt = bcrypt.gensalt(rounds=12)
+    hash_plain = bcrypt.hashpw(b_plain, salt)
+    return hash_plain.decode()
+
+
+def verify_password(plain: str, hashed: str) -> bool:
+    b_plain = plain.encode("utf-8")
+    b_hashed = hashed.encode("utf-8")
+    return bcrypt.checkpw(b_plain, b_hashed)
