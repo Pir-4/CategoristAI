b574d79e506f3b9a758aa9612e32656dcf42662d
Valentin Agapitov <valentin.agapitov@exness.com>
2026-03-23 20:33:20 +0000

    Create aurh schemas

---
+++ app/schemas/auth.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+
+
+class LoginRequest(BaseModel):
+    login: str = Field(min_length=1, max_length=30, title="User login")
+    password: str = Field(min_length=5, title="User password")
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
