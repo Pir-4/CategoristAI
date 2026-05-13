5e31a7464221b6b6697987ecd7bebc86b461e4bb
Valentin Agapitov <valentin.agapitov@exness.com>
2026-05-13 20:32:55 +0100

    Change and extend parser

--- app/services/bank_parser/revolut.py
+++ app/services/bank_parser/revolut.py
@@ -1,28 +1,51 @@
+import logging
 from datetime import datetime
 from decimal import Decimal
 
-from app.models import Transaction
+from app.core.constants import TransactionType
+from app.models import Account, Transaction
 
 from .base import BankParserBase
 
+logger = logging.getLogger(__name__)
+
 
 class RevolutParser(BankParserBase):
-    def parse(self, raw_transactions: list[dict]) -> list[Transaction]:
+    def parse(
+        self, raw_transactions: list[dict], accounts: list[Account]
+    ) -> list[Transaction]:
         result: list[Transaction] = []
-        for transaction in raw_transactions:
-            if transaction["Type"] != "Card Payment":
+        for raw_transaction in raw_transactions:
+            logger.debug(f"[Revolut] Raw transaction: {raw_transaction}")
+            if raw_transaction["State"] != "COMPLETED":
                 continue
 
-            if transaction["State"] != "COMPLETED":
-                continue
-
-            expense = Transaction(
-                date=datetime.strptime(
-                    transaction["Completed Date"], "%Y-%m-%d %H:%M:%S"
-                ),
-                amount=abs(Decimal(transaction["Amount"])),
-                merchant=transaction["Description"],
-                description=transaction["Description"],
+            description = raw_transaction["Description"]
+            account = self.get_account(accounts, description)
+            transaction_type = self.get_transaction_type(
+                account, raw_transaction
             )
-            result.append(expense)
+            is_categorizable = self.is_categorizable(transaction_type, account)
+            merchant = (
+                description
+                if transaction_type == TransactionType.EXPENSE
+                else None
+            )
+
+            transaction = Transaction(
+                date=datetime.strptime(
+                    raw_transaction["Completed Date"], "%Y-%m-%d %H:%M:%S"
+                ),
+                amount=Decimal(raw_transaction["Amount"]),
+                merchant=merchant,
+                description=description,
+                transaction_type=transaction_type,
+                account_id=account.id if account else None,
+                is_categorizable=is_categorizable,
+            )
+            result.append(transaction)
         return result
+
+    def is_income(self, raw_transaction: dict) -> bool:
+        amount = Decimal(raw_transaction["Amount"])
+        return amount > 0
